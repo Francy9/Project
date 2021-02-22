45e5d48340d0f3d39910120a87344cdefac0fbe1
Francesco <48328557+Francy9@users.noreply.github.com>
2021-02-22 12:35:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ $messaggio_prec = file_get_contents($last_message);
 
 $chatid = $update["message"]["from"]["id"];
 $text = $update["message"]["text"];
+$lat = $update["message"]["location"]["latitude"];
+$long =$update["message"]["location"]["longitude"];
 if($text=="/start"){
  $keyboard = '["Dettagiato"],["Giornaliero"]';
  sendMessage($chatid,"Ciao che tipo di meteo vuoi?",$keyboard);
@@ -24,7 +26,8 @@ switch($messaggio_prec){
   file_put_contents($last_message, $text);
   }else{
   if($text=="Giornaliero"){
-   sendMessage($chatid,"inserisci il luogo",$keyboard);
+    $keyboard = '["Oggi"],["Domani"],["Fra 2 giorni"],["Fra 3 giorni"],["Fra 4 giorni"],["Fra 5 giorni"],["Fra 6 giorni"],["Fra 7 giorni"]';
+   sendMessage($chatid,"fra quanti giorni?",$keyboard); 
    file_put_contents($last_message, $text);
    }else{
    $keyboard = '["Dettagiato"],["Giornaliero"]';
@@ -48,9 +51,23 @@ switch($messaggio_prec){
   }
   break;
   case "Giornaliero":
-   $meteo = openweather("Giornaliero",$text,$last_message);
-   sendMessage($chatid,$meteo,$tastiera);
-    
+  if ($text=="Oggi"){ 
+   sendMessage($chatid,"inserisci luogo o coordinate",$keyboard);
+   file_put_contents($last_message, $text);
+  }else 
+   if($text=="Domani"||
+      $text=="Fra 2 giorni"||
+      $text=="Fra 3 giorni"||
+      $text=="Fra 4 giorni"||
+      $text=="Fra 5 giorni"||
+      $text=="Fra 6 giorni"||
+      $text=="Fra 7 giorni"){
+   sendMessage($chatid,"inserisci coordinate",$keyboard);
+   file_put_contents($last_message, $text);
+   }else{
+    $keyboard = '["Oggi"],["Domani"],["Fra 2 giorni"],["Fra 3 giorni"],["Fra 4 giorni"],["Fra 5 giorni"],["Fra 6 giorni"],["Fra 7 giorni"]';
+   sendMessage($chatid,"non ho capito!",$keyboard);    
+   } 
  break;
  case "Tempo" :
   $meteo = openweather("Tempo",$text,$last_message);
@@ -80,6 +97,43 @@ switch($messaggio_prec){
    $meteo = openweather("Tramonto",$text,$last_message);
    sendMessage($chatid,$meteo,$tastiera);
   break;
+  case "Oggi" :
+  if($text==NULL){
+   $meteo = openweatherCoor("Giornaliero","0",$lat,$long,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  }else{
+   $meteo = openweather("Giornaliero",$text,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  }
+  break;
+  case "Domani" :
+   $meteo = openweatherCoor("Giornaliero","1",$lat,$long,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  break;
+  case "Fra 2 giorni" :
+   $meteo = openweatherCoor("Giornaliero","2",$lat,$long,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  break;
+  case "Fra 3 giorni" :
+   $meteo = openweatherCoor("Giornaliero","3",$lat,$long,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  break;
+  case "Fra 4 giorni" :
+   $meteo = openweatherCoor("Giornaliero","4",$lat,$long,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  break;
+  case "Fra 5 giorni" :
+   $meteo = openweatherCoor("Giornaliero","5",$lat,$long,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  break;
+  case "Fra 6 giorni" :
+   $meteo = openweatherCoor("Giornaliero","6",$lat,$long,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  break;
+  case "Fra 7 giorni" :
+   $meteo = openweatherCoor("Giornaliero","7",$lat,$long,$last_message);
+   sendMessage($chatid,$meteo,$tastiera);
+  break;
  default:
   sendMessage($chatid,"per iniziare digita /start",$keyboard);
    break;
@@ -97,6 +151,15 @@ Function openWeather($tipo,$luogo,$last_message){
  $risposta = http_request("https://server-openweather.herokuapp.com/$tipo/$luogo");
    if(is_numeric($risposta)){
      return("furbetto scrivi una città");
+     }
+     else{
+     file_put_contents($last_message, $luogo); 
+     return($risposta);
+     }
+Function openWeatherCoor($tipo,$giorno,$lat,$long,$last_message){
+ $risposta = http_request("https://server-openweather.herokuapp.com/$tipo/$giorno?lat=$lat&long=$long");
+   if(is_numeric($risposta)){
+     return("furbetto usa le coordinate");
      }
      else{
      file_put_contents($last_message, $luogo); 
